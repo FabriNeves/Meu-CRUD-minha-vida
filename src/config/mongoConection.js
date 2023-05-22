6a7586aa5f521dba62c7f45c7cd26fa4c90abdd4
FaSnow <eng.fbneves@gmail.com>
2023-05-22 17:22:38 -0300

    🐛 Correção de bugs para conexão do banco

--- src/config/mongoConection.js
+++ src/config/mongoConection.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://Usuario1:1234@to-do-list.xgrmd7m.mongodb.net/Tarefas");
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/Dados");
+//mongoose.connect("mongodb+srv://DadosUser:Z37Ds3xGjRMzeeuG@cluster23.paj9mri.mongodb.net/?retryWrites=true&w=majority");
+
 
 let db = mongoose.connection
 
