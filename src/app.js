82a6d91e59e57f9529685bc5b1ded2195222c7c1
FaSnow <eng.fbneves@gmail.com>
2023-05-24 17:51:27 -0300

    Add Cors

--- src/app.js
+++ src/app.js
@@ -4,13 +4,22 @@ import express from "express";
 import db from "./config/mongoConection.js";
 import routes from "./routes/index.js";
 import trataErro from "./middlewares/tratamentoDeErros.js";
+import cors from 'cors';
 
 db.on("error", console.log.bind(console, "Erro de conexão...")); // bind = ligar , conectar.
 db.once("open", () => console.log("Conexão com o banco de dados estabelecida."));
 
 const app = express();
+
+// Habilitar CORS
+app.use(cors({
+    origin: '*'
+}));
+
 app.use(express.json())
+
 routes(app);
+
 app.use((erro, req, res, next) => {
    trataErro(erro,res);
 })
